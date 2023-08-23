d3f8f2a6f911e00c6f40a0f3f8b989ef923513fa
Alberto Ceballos <albertoceballos20@gmail.com>
2023-08-23 17:16:28 -0500

    fix formatting

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -31,7 +31,7 @@ public_users.get('/',function (req, res) {
   const get_books = new Promise((resolve, reject) =>{
     resolve(res.send(JSON.stringify(books,null,4)));
     reject(res.json({"message": "error"}));
-  })
+  });
   
   get_books
   .then(()=>{console.log("promise resolved");})
@@ -55,7 +55,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   get_book_details
   .then(()=>{console.log("promise resolved");})
   .catch(error=>{console.log("Error: " + error);});
- });
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
